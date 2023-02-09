8a28db645ba171881a5be877801bbcc8fb84762e
Lujiang <lujiang0111@gmail.com>
2023-02-09 17:35:15 +0800

    格式化shell脚本

--- Programming/编译脚本/autobuild_cmake.sh
+++ Programming/编译脚本/autobuild_cmake.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-shell_path=$(cd "$(dirname "$0")";pwd)/
+shell_path=$(
+	cd "$(dirname "$0")" || exit
+	pwd
+)/
 project=example_lib
 version=1.0.0
 full_version=v${version}-release
@@ -19,31 +22,30 @@ rm -rf ${install_version_path}
 echo -e "done!"
 echo -e "\n\033[33m============= installing =============\033[0m\n"
 
-cd ${src_path}
+cd "${src_path}" || exit
 rm -rf build/${os_version}/
 mkdir -p build/${os_version}/
 
-cd build/${os_version}/
+cd build/${os_version}/ || exit
 cmake -DCMAKE_INSTALL_PREFIX=${install_version_path} ../..
-make clean && make V=1 -j$(nproc) && make install
+make clean && make V=1 -j"$(nproc)" && make install
 
 echo -e "done!"
 echo -e "\n\033[33m========== do some cleaning ==========\033[0m\n"
 
-rm -rf ${dst_path}
-mkdir -p ${dst_path}include
-mkdir -p ${dst_path}lib
+rm -rf "${dst_path}"
+mkdir -p "${dst_path}"include
+mkdir -p "${dst_path}"lib
 
-\cp -rf ${install_version_path}include/* ${dst_path}include/
-\cp -rf ${install_version_path}lib/* ${dst_path}lib/
+\cp -rf ${install_version_path}include/* "${dst_path}"include/
+\cp -rf ${install_version_path}lib/* "${dst_path}"lib/
 
-cd ${dst_path}lib/
-for src_file in $(find ./ -name "*.so*")
-do
-	dst_file=$(readlink ${src_file})
+cd "${dst_path}"lib/ || exit
+find ./ -name "*.so*" -print0 | while IFS= read -r -d '' src_file; do
+	dst_file=$(readlink "${src_file}")
 
 	if [[ ${dst_file}"x" != "x" ]]; then
-		rm -f ${src_file}
+		rm -f "${src_file}"
 	fi
 done
 
