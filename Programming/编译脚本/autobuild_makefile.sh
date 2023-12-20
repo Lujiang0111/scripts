a1801d4dc168af79f804c821760a46a6127cb89c
Lujiang <lujiang0111@gmail.com>
2023-12-20 11:15:23 +0800

    自动判断os_version与os_arch

--- Programming/编译脚本/autobuild_makefile.sh
+++ Programming/编译脚本/autobuild_makefile.sh
@@ -6,14 +6,46 @@ shell_path=$(
 project=example_lib
 version=1.0.0
 full_version=v${version}-release
-os_version=centos7.1
-os_arch=x64
-src_path=${shell_path}../../../../src/
-dst_path=${shell_path}../../../../../../../../Versions/Baselib/${project}/v${version}/linux/${os_version}/${os_arch}/
 
 install_root_path=/home/install/
 install_project_path=${install_root_path}${project}/
 install_version_path=${install_project_path}${full_version}/
+
+#$1: os version
+os_version_default=linux/centos7.1/
+os_version=
+if [ -n "$1" ]; then
+	os_version=linux/$1/
+else
+	if grep "Ubuntu" /etc/os-release; then
+		os_version=linux/ubuntu22.04/
+	elif grep "Kylin" /etc/os-release; then
+		os_version=linux/KylinV10/
+	else
+		os_version=${os_version_default}
+	fi
+fi
+echo -e "os_version=\033[34m${os_version}\033[0m"
+
+#$2: os arch
+os_arch_default=x64
+os_arch=
+if [ -n "$2" ]; then
+	os_arch=$2
+else
+	uname_ret=$(uname -a)
+	if [[ ${uname_ret} == *"x86_64"* ]]; then
+		os_arch=x64
+	elif [[ ${uname_ret} == *"aarch64"* ]]; then
+		os_arch=aarch64
+	else
+		os_arch=${os_arch_default}
+	fi
+fi
+echo -e "os_arch=\033[34m${os_arch}\033[0m"
+
+src_path=${shell_path}../../../../src/
+dst_path=${shell_path}../../../../../../../../Versions/Baselib/${project}/v${version}/linux/${os_version}/${os_arch}/
 
 echo -e "\n\033[33m============= preparing =============\033[0m\n"
 
