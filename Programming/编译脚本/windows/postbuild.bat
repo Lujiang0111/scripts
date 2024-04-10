bce1b6975e8c6bff32eb351323e0ef48ae25ceb1
Lujiang <lujiang0111@gmail.com>
2024-04-10 15:56:55 +0800

    更改脚本位置

---
+++ Programming/编译脚本/windows/postbuild.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set bin_base=..\..\bin\
+
+if not exist %bin_base% (
+    echo %bin_base% not exist
+    goto :error
+)
+
+set src_base=..\..\..\..\
+
+set lib_name=libflow
+if exist %src_base%%lib_name% (
+    xcopy %src_base%%lib_name%\bin %bin_base% /S /Y /C
+) else (
+    echo please put https://github.com/Lujiang0111/%lib_name% in %src_base%
+    goto :error
+)
+
+endlocal
+exit /b 0
+
+:error
+endlocal
+exit /b 1
--- Programming/编译脚本/windows/prebuild.bat
+++ Programming/编译脚本/windows/prebuild.bat
@@ -10,7 +10,7 @@ rmdir /Q /S %dep_base%
 mkdir %dep_base%include
 mkdir %dep_base%lib
 
-set src_base=..\..\..\
+set src_base=..\..\..\..\
 
 set lib_name=libflow
 if exist %src_base%%lib_name% (
@@ -18,7 +18,7 @@ if exist %src_base%%lib_name% (
     xcopy %src_base%%lib_name%\include %dep_base%include\%lib_name% /S /Y /C
     xcopy %src_base%%lib_name%\bin %dep_base%lib /S /Y /C
 ) else (
-    echo please put https://github.com/Lujiang0111/libflow in %src_base%
+    echo please put https://github.com/Lujiang0111/%lib_name% in %src_base%
     goto :error
 )
 
